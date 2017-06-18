2a7f0cc92d7d125193be4e734ead8b216867504d
Emily Chen <chenemily3@yahoo.com>
2017-06-18 04:01:23 -0400

    Set up split screen, basic movement, health ui

---
+++ BreadCactusProj/Assets/CameraFollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+
+    //drag either bread or cactus gameobject here
+    public GameObject player;
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        //clamp?
+    }
+}
---
+++ BreadCactusProj/Assets/PlayerMovement.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public enum PlayerType
+    {
+        Bread, Cactus
+    }
+
+    public PlayerType playerType;
+    private KeyCode leftKey;
+    private KeyCode rightKey;
+    private KeyCode upKey;
+    private KeyCode downKey;
+
+    public float speed = 5f;
+    public float jump = 5f;
+
+    private bool grounded;
+    private float raycastLength;    //cactus is taller than bread, so it will need a longer raycast length
+
+    private bool currentDirection = false;  //left is false, right is true
+    private bool newDirection = false;
+
+    private Rigidbody2D rbody;
+
+    void Awake () {
+		if (playerType == PlayerType.Bread)
+        {
+            leftKey = KeyCode.A;
+            rightKey = KeyCode.D;
+            upKey = KeyCode.W;
+            downKey = KeyCode.S;
+            raycastLength = 2.3f;
+        } else
+        {
+            leftKey = KeyCode.LeftArrow;
+            rightKey = KeyCode.RightArrow;
+            upKey = KeyCode.UpArrow;
+            downKey = KeyCode.DownArrow;
+            raycastLength = 3.2f;
+        }
+
+        rbody = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        //Change direction based on the last key (left or right) pressed
+        if (currentDirection != newDirection)
+        {
+            transform.Rotate(new Vector3(0, -180, 0));
+            currentDirection = newDirection;
+        }
+
+        Walk();
+        Jump();
+	}
+
+    private void CheckGrounded()
+    {
+        //Vector3 end = new Vector3(transform.position.x, transform.position.y - 2.3f, 0);
+        //Debug.DrawLine(transform.position, end, Color.red);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastLength);
+        if (hit.collider != null && hit.collider.tag == "Platforms")
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
+
+    private void Walk()
+    {
+        if (Input.GetKey(leftKey))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed, Space.World);
+            newDirection = false;
+        }
+        if (Input.GetKey(rightKey))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed, Space.World);
+            newDirection = true;
+        }
+    }
+
+    private void Jump()
+    {
+        CheckGrounded();
+        if (Input.GetKeyDown(upKey) && grounded)
+        {
+            rbody.velocity = Vector2.up * jump;
+        }
+    }
+}
